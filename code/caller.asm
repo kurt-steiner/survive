aecfd045284843fd654b6701af6e604490141271
steiner <steiner3044@163.com>
2026-03-15 20:17:31 +0800

    storage 3.15, 更改了目录结构

---
+++ code/caller.asm
@@ -0,0 +1,35 @@
+	.file	"caller.c"
+	.text
+	.globl	add
+	.type	add, @function
+add:
+	pushq	%rbp
+	movq	%rsp, %rbp
+	movl	%edi, -4(%rbp)
+	movl	%esi, -8(%rbp)
+	movl	-4(%rbp), %edx
+	movl	-8(%rbp), %eax
+	addl	%edx, %eax
+	popq	%rbp
+	ret
+	.size	add, .-add
+	.globl	main
+	.type	main, @function
+main:
+	pushq	%rbp
+	movq	%rsp, %rbp
+	subq	$16, %rsp
+	movl	$125, -4(%rbp)
+	movl	$80, -8(%rbp)
+	movl	-8(%rbp), %edx
+	movl	-4(%rbp), %eax
+	movl	%edx, %esi
+	movl	%eax, %edi
+	call	add
+	movl	%eax, -12(%rbp)
+	movl	-12(%rbp), %eax
+	leave
+	ret
+	.size	main, .-main
+	.ident	"GCC: (Debian 14.2.0-19) 14.2.0"
+	.section	.note.GNU-stack,"",@progbits
